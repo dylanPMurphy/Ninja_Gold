c5b968289ef8b59bdfc6822acf7695d8ec999213
dylanPMurphy <dylanmurphydba@gmail.com>
2020-09-26 14:39:45 -0700

    finished

--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -4,22 +4,34 @@ import random
 def index(request):
     if 'gold' not in request.session:
         request.session['gold'] = 0
+    if 'activities' not in request.session:
+        request.session['activities'] = []
     return render(request, "index.html")
 
 def process_money(request):
     print('The form has been submitted')
     print(request.POST)
     if request.POST['building'] == 'farm':
-        request.session['gold']+= random.randint(10,20)
-        print("the farm has been visited")
+        num = random.randint(10,20)
+        request.session['gold']+= num
+        request.session['activities'].append("You earned "+ str(num) +"gold!" )
     elif request.POST['building'] == 'cave':
-        request.session['gold']+= random.randint(5,10)
-        print("the cave has bee visited")
+        num = random.randint(5,10)
+        request.session['gold']+= num
+        request.session['activities'].append("You earned "+ str(num) +"gold!" )
     elif request.POST['building'] == 'house':
-        request.session['gold']+= random.randint(2,5)
-        print("the house has bee visited")
-    elif request.POST['building'] == 'casino':
-        request.session['gold']+= random.randint(-50,50)
-        print("the casino has bee visited")
+        num = random.randint(2,5)
+        request.session['gold']+= num
+        request.session['activities'].append("You earned "+ str(num) +"gold!" )
+    elif request.POST['building'] == 'casino': 
+        num = random.randint(-50,50)
+        if num > 0:
+            request.session['gold'] += num
+            request.session['activities'].append("You earned "+ str(num)+ " gold")
+        elif num < 0:
+            request.session['gold'] += num
+            request.session['activities'].append("You lost "+ str(num)+ " gold")
+        else:
+            request.session['activities'].append("You broke even")
     print('You now have '+str(request.session['gold']) +'gold')
     return redirect('/') 
