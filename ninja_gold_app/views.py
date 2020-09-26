916b71b446e34002b5134a8a9e996e85fbe6dd94
dylanPMurphy <dylanmurphydba@gmail.com>
2020-09-25 18:12:57 -0700

    4 forms are working and can distinguish in views

--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import random
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def process_money(request):
+    print('The form has been submitted')
+    print(request.POST)
+    if request.POST['building'] == 'farm':
+        print("the farm has been visited")
+    elif request.POST['building'] == 'cave':
+        print("the cave has bee visited")
+    elif request.POST['building'] == 'house':
+        print("the house has bee visited")
+    elif request.POST['building'] == 'casino':
+        print("the casino has bee visited")
+    
+    return redirect('/') 
